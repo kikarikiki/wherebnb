7ff7b417b36c32982e1ef354d73625e0e43a88e5
Antje Stiller <68593523+kikarikiki@users.noreply.github.com>
2023-09-18 14:24:08 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './app.scss'
+import Header from './components/header/Header'
+import Home from './components/home/Home'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+export default function App() {
+
+  return (
+    <div>
+      <Header />
+      <Home />
+    </div>
+  )
+}
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './header.scss';
+
+export default function Header() {
+
+  return (
+    <div className='test'>test</div>
+  )
+}
