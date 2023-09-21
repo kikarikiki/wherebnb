8434d2cf3bdaea5f8a866ebeba38f443587887aa
Antje Stiller <antjestiller@gmx.de>
2023-09-21 08:44:23 +0200

    Pass props to the Card component and display that data

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -1,22 +1,22 @@
 import React from 'react';
 import './cards.scss';
 
-export default function Card() {
+export default function Card(props) {
 
   return (
     <>
       <div className="card col-sm-8 col-md-6 col-lg-3 mb-3 p-0">
         <span className='booking-status position-absolute badge bg-light text-dark'>SOLD OUT</span>
-        <img src="../../src/assets/images/katie-zaferes.png" className="card-img-top" alt="..." />
+        <img src={props.img} className="card-img-top" alt="..." />
         <div className="card-body">
           <div className='card-stats d-flex align-items-center text-secondary'>
             <i><img src="../../src/assets/images/star.png" alt="" /></i>
-            <span className="card-rating">3</span>
-            <span className="card-reviews">(3) • </span>
-            <span className="country">USA</span>
+            <span className="card-rating">{props.rating}</span>
+            <span className="card-reviewCount">({props.reviewCount}) • </span>
+            <span className="country">{props.country}</span>
           </div>
-          <p className="card-text">Life lessons with Katie Zaferes</p>
-          <p className="card-price-person"><strong>From 99€ </strong><small className="text-secondary">/ person</small></p>
+          <p className="card-text">{props.title}</p>
+          <p className="card-price-person"><strong>From {props.price}€ </strong><small className="text-secondary">/ person</small></p>
         </div>
       </div>
     </>
--- src/components/cards/CardCaroussel.jsx
+++ src/components/cards/CardCaroussel.jsx
@@ -5,10 +5,38 @@ export default function CardCaroussel() {
 
   return (
     <section className="row g-2">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      <Card
+        img="../../../src/assets/images/katie-zaferes.png"
+        title="Life Lessons with Katie Zaferes"
+        rating="5.0"
+        reviewcount={6}
+        country="USA"
+        price={99}
+      />
+      <Card
+        img="../../../src/assets/images/katie-zaferes.png"
+        title="Life Lessons with Katie Zaferes"
+        rating="5.0"
+        reviewcount={6}
+        country="USA"
+        price={99}
+      />
+      <Card
+        img="../../../src/assets/images/katie-zaferes.png"
+        title="Life Lessons with Katie Zaferes"
+        rating="5.0"
+        reviewcount={6}
+        country="USA"
+        price={99}
+      />
+      <Card
+        img="../../../src/assets/images/katie-zaferes.png"
+        title="Life Lessons with Katie Zaferes"
+        rating="5.0"
+        reviewcount={6}
+        country="USA"
+        price={99}
+      />
     </section>
   )
 }
