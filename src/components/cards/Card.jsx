e436347dea3d1f86e1f02f1aa698fb88d6b25dfc
Antje Stiller <antjestiller@gmx.de>
2023-09-21 11:24:56 +0200

    data into components

--- src/components/cards/Card.jsx
+++ src/components/cards/Card.jsx
@@ -7,13 +7,13 @@ export default function Card(props) {
     <>
       <div className="card col-sm-8 col-md-6 col-lg-3 mb-3 p-0">
         <span className='booking-status position-absolute badge bg-light text-dark'>SOLD OUT</span>
-        <img src={props.img} className="card-img-top" alt="..." />
+        <img src={`../../public/images/${props.coverImg}`} className="card-img-top" alt="..." />
         <div className="card-body">
           <div className='card-stats d-flex align-items-center text-secondary'>
-            <i><img src="../../src/assets/images/star.png" alt="" /></i>
+            <i><img src="../../public/images/star.png" alt="" /></i>
             <span className="card-rating">{props.rating}</span>
             <span className="card-reviewCount">({props.reviewCount}) • </span>
-            <span className="country">{props.country}</span>
+            <span className="country">{props.location}</span>
           </div>
           <p className="card-text">{props.title}</p>
           <p className="card-price-person"><strong>From {props.price}€ </strong><small className="text-secondary">/ person</small></p>
--- src/components/cards/CardCaroussel.jsx
+++ src/components/cards/CardCaroussel.jsx
@@ -1,42 +1,25 @@
 import React from 'react';
 import Card from './Card';
+import data from '../../data';
 
-export default function CardCaroussel() {
-
+export default function CardCaroussel(){
+  // <Hero />
+const cards = data.map(item => {
   return (
-    <section className="row g-2">
-      <Card
-        img="../../../src/assets/images/katie-zaferes.png"
-        title="Life Lessons with Katie Zaferes"
-        rating="5.0"
-        reviewcount={6}
-        country="USA"
-        price={99}
-      />
-      <Card
-        img="../../../src/assets/images/katie-zaferes.png"
-        title="Life Lessons with Katie Zaferes"
-        rating="5.0"
-        reviewcount={6}
-        country="USA"
-        price={99}
-      />
-      <Card
-        img="../../../src/assets/images/katie-zaferes.png"
-        title="Life Lessons with Katie Zaferes"
-        rating="5.0"
-        reviewcount={6}
-        country="USA"
-        price={99}
-      />
-      <Card
-        img="../../../src/assets/images/katie-zaferes.png"
-        title="Life Lessons with Katie Zaferes"
-        rating="5.0"
-        reviewcount={6}
-        country="USA"
-        price={99}
-      />
-    </section>
+    <Card
+        coverImg={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+    />
   )
+})
+
+return (
+<section className="row g-2">
+  {cards}
+</section>
+)
 }
